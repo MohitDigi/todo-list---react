cc387b82e54b6131e86d1ff3cbd3ad776178e978
Mohit Kalshan <mohit.kalshan@digimantra.com>
2023-06-20 10:31:12 +0530

    Routes and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes ,  Route } from "react-router-dom";
+import DisplayItem from "./component/DisplayItem";
+import Home from "./component/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/display-item" element={<DisplayItem />} />
+      </Routes>
+    </>
   );
 }
 
